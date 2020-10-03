aaa6eed7588927ed4ccf451dfbb4c97c1a2744ad
Pooja-Y-G <poojayg22@gmail.com>
2020-10-03 15:14:27 +0530

    typescript commit

---
+++ src/API.ts
@@ -0,0 +1,36 @@
+import { shuffleArray } from "./util";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+//after console.log of fetchQuizquestions got to know the fields of question object so defining the type ofeach
+
+export type QuestionState = Question & { answers: string[] };
+// this type takes a question type above and add answers i,e string of array to join incrrect and correct answer together
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
